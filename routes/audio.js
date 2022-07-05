4b3bd42159552e4f7eab9180e8deb4d128416dca
Thomas Daley <prayerproduction@gmail.com>
2022-07-05 13:40:12 +0100

    modified env

--- routes/audio.js
+++ routes/audio.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const PORT = 8080;
-const url = process.env.API_URI;
+const url = process.env.API_URI + process.env.PORT;
 
 router.get("/atmospheric", (req, res) => {
   res.status(200).json({
     id: "1",
     name: "atmospheric",
-    bass: `${url}:${PORT}/audio/atmospheric-bass.wav`,
-    pad1: `${url}:${PORT}/audio/atmospheric-pad1.wav`,
-    pad2: `${url}:${PORT}/audio/atmospheric-pad2.wav`,
-    arp: `${url}:${PORT}/audio/atmospheric-arp.wav`,
-    lead1: `${url}:${PORT}/audio/atmospheric-lead1.wav`,
-    lead2: `${url}:${PORT}/audio/atmospheric-lead2.wav`,
-    lead3: `${url}:${PORT}/audio/atmospheric-lead3.wav`,
-    drone: `${url}:${PORT}/audio/atmospheric-drone.wav`,
+    bass: `${url}/audio/atmospheric-bass.wav`,
+    pad1: `${url}/audio/atmospheric-pad1.wav`,
+    pad2: `${url}/audio/atmospheric-pad2.wav`,
+    arp: `${url}/audio/atmospheric-arp.wav`,
+    lead1: `${url}/audio/atmospheric-lead1.wav`,
+    lead2: `${url}/audio/atmospheric-lead2.wav`,
+    lead3: `${url}/audio/atmospheric-lead3.wav`,
+    drone: `${url}/audio/atmospheric-drone.wav`,
   });
 });
 
@@ -22,14 +22,14 @@ router.get("/hyacintho", (req, res) => {
   res.status(200).json({
     id: "2",
     name: "atmospheric",
-    bass: `${url}:${PORT}/audio/hyacintho-bass.wav`,
-    pad1: `${url}:${PORT}/audio/hyacintho-pad1.wav`,
-    pad2: `${url}:${PORT}/audio/hyacintho-pad2.wav`,
-    arp: `${url}:${PORT}/audio/hyacintho-arp.wav`,
-    lead1: `${url}:${PORT}/audio/hyacintho-lead1.wav`,
-    lead2: `${url}:${PORT}/audio/hyacintho-lead2.wav`,
-    lead3: `${url}:${PORT}/audio/hyacintho-lead3.wav`,
-    drone: `${url}:${PORT}/audio/hyacintho-drone.wav`,
+    bass: `${url}/audio/hyacintho-bass.wav`,
+    pad1: `${url}/audio/hyacintho-pad1.wav`,
+    pad2: `${url}/audio/hyacintho-pad2.wav`,
+    arp: `${url}/audio/hyacintho-arp.wav`,
+    lead1: `${url}/audio/hyacintho-lead1.wav`,
+    lead2: `${url}/audio/hyacintho-lead2.wav`,
+    lead3: `${url}/audio/hyacintho-lead3.wav`,
+    drone: `${url}/audio/hyacintho-drone.wav`,
   });
 });
 
