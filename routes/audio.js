4efeabb34ab0b591f0213062939aa42b80ae2b02
Thomas Daley <prayerproduction@gmail.com>
2022-07-07 10:00:09 +0100

    added port back up option

--- routes/audio.js
+++ routes/audio.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const PORT = 8080;
-const url = process.env.API_URI;
+const url = process.env.API_URI || 8080;
 
 router.get("/", (req, res) => {
   res
