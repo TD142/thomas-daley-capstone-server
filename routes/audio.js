1510828165d8ae49d6b740fe7686f1607196ed93
Thomas Daley <prayerproduction@gmail.com>
2022-12-31 09:56:09 +0000

    env test for render hosting

--- routes/audio.js
+++ routes/audio.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const url = process.env.API_URI || "http://localhost:8080";
+const url = process.env.API_URI;
 
 router.get("/", (req, res) => {
   res
